cad4b969c61062ec185858c30e9c011414aca205
GitKilla <robertnclark1@gmail.com>
2018-02-28 19:42:17 -0500

    wattap

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports =  {
-    GoogleClientID:'216236258110-b364d756bcd99f02rqkkt08t1fg2ikeg.apps.googleusercontent.com',
-    GoogleClientSecret:'SP7aG6sTILyv8KDCFQ9O5fB6',
-    FacebookClientID: 's',
-    FacebookClientSecret: 's',
-    cookieKey: 'dfdfdfdfdfdfd777d77ddddppppllwllwww'
+    GoogleClientID: node.env.GoogleClientID,
+    GoogleClientSecret: node.env.GoogleClientSecret,
+    FacebookClientID: node.env.FacebookClientID,
+    FacebookClientSecret: node.env.FacebookClientSecret,
+    cookieKey: node.env.cookieKey
 }
